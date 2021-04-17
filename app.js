848b59c35ddf9c829779524caea33a0b46cf6414
ROBERTO PUPO <pupor@hotmail.com>
2021-04-17 15:00:05 -0500

    _4th commit

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-console.log("app.js loaded")
+console.log("app.js loaded");
+
+function DrawBargraph(sampleId){
+    console.log(`DrawBargraph(${sampleId})`);
+
+}
+
+function DrawBublechart(sampleId){
+    console.log(`DrawBublechart(${sampleId})`);
+    
+}
+
+function ShowMetadata(sampleId){
+    console.log(`ShowMetadata(${sampleId})`);
+    
+}
+
+function optionChanged(newSampleId){
+    console.log(`User selected ${newSampleId} `);
+
+    DrawBargraph(newSampleId);
+    DrawBublechart(newSampleId);
+    ShowMetadata(newSampleId);
+}
 
 function InitDashboard(){
     console.log("InitDashboard()")
@@ -16,6 +39,12 @@ function InitDashboard(){
                 .text(sampleId)
                 .property("value", sampleId);
         });
+
+        var id = samplesNames[0];
+
+        DrawBargraph(id);
+        DrawBublechart(id);
+        ShowMetadata(id);
 
 
     });
