881df921bc642a85233e09b66af50c9a3d59704e
ROBERTO PUPO <pupor@hotmail.com>
2021-04-24 14:03:34 -0500

    Completed two graph

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ function DrawBargraph(sampleId){
 
     d3.json("data/samples.json").then(data => {
         //
+        console.log("data contains ...");
+        console.log(data);
 
         var samples = data.samples;
         var resultArray = samples.filter(s => s.id == sampleId);
 
+        console.log("resultArray contains ...");
         console.log(resultArray);
 
         var result = resultArray[0];
 
+        console.log("result contains ...");
         console.log(result);
 
         var otu_ids = result.otu_ids;
@@ -27,13 +31,13 @@ function DrawBargraph(sampleId){
 
         console.log(sample_values);
 
-        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`);
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
 
         var barData = {
-            x: sample_values.slice(0,10),
+            x: sample_values.slice(0,10).reverse(),
             y: yticks,
             type: "bar",
-            text: otu_labels.slice(0,10),
+            text: otu_labels.slice(0,10).reverse(),
             orientation: "h"
 
         };
@@ -78,10 +82,10 @@ function DrawBubbleChart(sampleId){
 
         console.log(sample_values);
 
-        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`);
+        yticks = sample_values.slice(0,10);
 
         var bubbleChartData = {
-            x: sample_values.slice(0,10),
+            x: otu_ids.slice(0,10).map(otuId => `${otuId}`),
             y: yticks,
             mode: "markers",
             text: otu_labels.slice(0,10),
@@ -90,15 +94,16 @@ function DrawBubbleChart(sampleId){
                 color: otu_ids,
                 colorscale: "Earth"
               }
-
         };
 
         var bubbleArray = [bubbleChartData];
 
         var bubbleChartLayout = {
             title: "Top 10 Bacteria Cultures Found",
-            margin: {t: 30, l: 150}
-
+            margin: {t: 0},
+            hovermode: "closest", // Default mode
+            xaxis: { title: "OTU ID" },
+            margin: { t: 30}            
         };
 
         Plotly.newPlot("bubble",bubbleArray, bubbleChartLayout);
@@ -107,8 +112,33 @@ function DrawBubbleChart(sampleId){
     
 }
 
+function DrawGaugeChart(sampleId){
+    console.log(`ShowMetadata("In DrawGaugeChart SampleID=" + ${sampleId})`);
+}
+
 function ShowMetadata(sampleId){
-    console.log(`ShowMetadata(${sampleId})`);
+    console.log(`ShowMetadata("In ShowMetadata SampleID=" + ${sampleId})`);
+    d3.json("data/samples.json").then((data) => {
+        var metadata = data.metadata;
+        // Select data for the object with the selected sampleId
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sampleId);
+        // Selecting first member in the array
+        var result = resultArray[0];
+        // Using d3 to select the panel tag with id of `#sample-metadata` in index.html
+        var demoPanel = d3.select("#sample-metadata");
+    
+        // Using .html("") to clear previous data
+        demoPanel.html("");
+    
+        // Add each key and value pair to the panel tag
+        Object.entries(result).forEach(([key, value]) => {
+          demoPanel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+          demoPanel.append("hr");
+        });
+    
+        // BONUS: Build the Gauge Chart
+        DrawGaugeChart(result.wfreq);
+      });
     
 }
 
@@ -137,16 +167,16 @@ function InitDashboard(){
                 .property("value", sampleId);
         });
 
-        var id = samplesNames[0];        
+        var initialId = samplesNames[0];        
 
         // Show or Update the bargraph
-        DrawBargraph(id);
+        DrawBargraph(initialId);
         
         // Update the bubblechart
-        DrawBubbleChart(id);
+        DrawBubbleChart(initialId);
 
         // Show or Update the demographic Information
-        ShowMetadata(id);
+        ShowMetadata(initialId);
 
     });
 
